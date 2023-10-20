838f077c5d1c5fdddfde1bf440c3077c3a4a5d26
Maxzolythus <bludoktor@gmail.com>
2023-10-20 09:11:41 -0500

    Fixing theming with mui

---
+++ src/app/assets/theme.tsx
@@ -0,0 +1,15 @@
+import { createTheme } from "@mui/material"
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        background: {
+            default: '#000000'
+        },
+        primary: {
+            main: '#8b0000'
+        },
+    },
+})
+
+export default theme
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Toolbar from './components/toolbar/toolbar'
+import ThemeRegistry from './util/themeRegistry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,8 +20,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Toolbar />
-        {children}
+        <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
       </body>
     </html>
   )
 }
+
