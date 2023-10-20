ee3a87b1a5aad99416d5832a067402544101f988
Maxzolythus <bludoktor@gmail.com>
2023-10-20 11:31:21 -0500

    Styling the form

--- src/app/simple/page.tsx
+++ src/app/simple/page.tsx
@@ -1,72 +1,96 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import Grid from '@mui/material/Grid'
 import InputLabel from '@mui/material/InputLabel'
 import MenuItem from '@mui/material/MenuItem'
 import FormControl from '@mui/material/FormControl'
-import Select, { SelectChangeEvent } from '@mui/material/Select'
+import Select from '@mui/material/Select'
 import TextField from '@mui/material/TextField'
 import CharacterPortrait from '../components/characterPortrait/characterPortrait'
 import { CHARACTERS, TEXTDISPLAYTYPES, POSES } from '../util/constants'
+import { Switch } from '@mui/base'
+import { Button, FormControlLabel, FormGroup, Stack } from '@mui/material'
+import styles from './styles.module.css'
 
 export default function Simple() {
-  const [ selectedCharacter, setSelectedCharacter ] = useState(CHARACTERS[0])
+  const [ selectedCharacter, setSelectedCharacter ] = useState<string>(CHARACTERS[0])
+  const [ selectedTextDisplayType, setSelectedTextDisplayType ] = useState(TEXTDISPLAYTYPES[0])
+  const [ selectedPose, setSelectedPose ] = useState(POSES[selectedCharacter] != undefined ? POSES[selectedCharacter][0] : undefined )
+  const [ enhancedSprites, setEnhancedSprites ] = useState(true)
   
   return (
-    <div>
-      {selectedCharacter}
-      <div className="container">
-        <Grid container spacing={2} columns={16}>
+    <div className={styles.container}>
+      <form className={styles.form} onSubmit={() => alert(`${selectedCharacter} ${selectedTextDisplayType} ${selectedPose} ${enhancedSprites}`)}>
+        <FormControl>
+          <FormControlLabel 
+            control={<Switch checked={enhancedSprites} onChange={() => setEnhancedSprites((prev) => !prev)} />} 
+            label="Enhanced Sprites?"
+            labelPlacement='start' />
+        </FormControl>
+        <Stack spacing={2}>
+        <Grid container spacing={2} columns={6}>
           {CHARACTERS.map((character) => {
             return (
-              <Grid item xs={2}>
+              <Grid key={character} item xs={2}>
                 <CharacterPortrait onClick={setSelectedCharacter} characterName={character} />
               </Grid>
             )
           })}
         </Grid>
+        
+        <FormGroup>
+        <Stack spacing={2}>
+          <FormControl>
+            <InputLabel id="text-display-type-label">Text Display Type</InputLabel>
+            <Select
+              labelId="text-display-type-label"
+              id="text-display-type-select"
+              value={selectedTextDisplayType}
+              label="Text Display Type"
+              onChange={(e) => setSelectedTextDisplayType(e.target.value)}
+            >
+              {TEXTDISPLAYTYPES.map((type) => {
+                return (
+                  <MenuItem key={type} value={type}>{type}</MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
 
-        <FormControl fullWidth>
-          <InputLabel id="text-display-type-label">Text Display Type</InputLabel>
-          <Select
-            labelId="text-display-type-label"
-            id="text-display-type-select"
-            value={TEXTDISPLAYTYPES[0]}
-            label="Text Display Type"
-            //onChange={handleChange}
-          >
-            {TEXTDISPLAYTYPES.map((type) => {
-              return (
-                <MenuItem value={type}>{type}</MenuItem>
-              )
-            })}
-          </Select>
-
-          <InputLabel id="pose-label">Pose</InputLabel>
-          <Select
-            labelId="pose-label"
-            id="pose-select"
-            label="Pose"
-            //onChange={handleChange}
-          >
-            {POSES[selectedCharacter]?.map((pose) => {
-              return (
-                <MenuItem value={pose}>{pose}</MenuItem>
-              )
-            })}
-          </Select>
+          <FormControl>
+            <InputLabel id="pose-label">Pose</InputLabel>
+            <Select
+              labelId="pose-label"
+              id="pose-select"
+              label="Pose"
+              value={selectedPose}
+              onChange={(e) => setSelectedPose(e.target.value)}
+            >
+              {POSES[selectedCharacter]?.map((pose) => {
+                return (
+                  <MenuItem key={pose} value={pose}>{pose}</MenuItem>
+                )
+              })}
+            </Select>
+          </FormControl>
+        
 
           <TextField
-          id="multiline-textbox"
-          label="Image Text"
-          multiline
-          rows={4}
-          defaultValue="Enter your text here..."
-        />
-        </FormControl>
-      </div>
-      <p>SIMPLE</p>
+            id="multiline-textbox"
+            label="Image Text"
+            multiline
+            rows={4}
+            defaultValue="Enter your text here..."
+          />
+          </Stack>
+        
+          <Button type="submit">
+            Submit
+          </Button>
+        </FormGroup>
+        </Stack>
+      </form>
     </div>
   )
 }
