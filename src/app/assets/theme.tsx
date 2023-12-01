565abc573456c1b45cf13252b564ba5b06a86134
Maxzolythus <bludoktor@gmail.com>
2023-12-01 14:45:14 -0600

    Adding text display types

--- src/app/assets/theme.tsx
+++ src/app/assets/theme.tsx
@@ -1,5 +1,12 @@
 import { createTheme } from "@mui/material"
 
+declare module '@mui/material/Paper' {
+    interface PaperPropsVariantOverrides {
+      overlay: true;
+      text: true;
+    }
+  }
+
 const theme = createTheme({
     palette: {
         mode: 'dark',
@@ -10,6 +17,30 @@ const theme = createTheme({
             main: '#8b0000'
         },
     },
+    components: {
+        MuiPaper: {
+            variants: [ 
+                {
+                    props: { variant: 'overlay'},
+                    style: {
+                        opacity: '.5',
+                        position: 'absolute',
+                        width: '500px',
+                        height: '500px'
+                    }
+                },
+                {
+                    props: { variant: 'text'},
+                    style: {
+                        opacity: '.5',
+                        position: 'absolute',
+                        width: '500px',
+                        height: '100px'
+                    }
+                }
+            ]
+        }
+    }
 })
 
 export default theme
--- src/app/simple/preview.tsx
+++ src/app/simple/preview.tsx
@@ -1,6 +1,12 @@
 import Container from '@mui/material/Container'
+import Image from 'next/image'
+import RoseGarden from '../assets/backgrounds/rose_garden.png'
 import { useState } from "react"
-import { ORIGINAL, TEXTDISPLAYTYPES, UPDATED } from '../util/constants'
+import { BACKGROUNDIMAGES, ORIGINAL, TEXTDISPLAYTYPES, UPDATED } from '../util/constants'
+import { Paper, Stack } from '@mui/material'
+
+// const backgroundImages = require.context('../assets/background', true, /\.png$/)
+// const imagePaths = backgroundImages.keys()
 
 type TextProps = {
     text: string
@@ -8,19 +14,19 @@ type TextProps = {
 
 function Overlay({ text }: TextProps) {
     return (
-        <>
+        <Paper variant='overlay'>
             <p>Overlay</p>
             <p>{text}</p>
-        </>
+        </Paper>
     )
 }
 
 function Speech({ text }: TextProps) {
     return (
-        <>
+        <Paper variant='text'>
             <p>Speech</p>
             <p>{text}</p>
-        </>
+        </Paper>
     )
 }
 
@@ -36,7 +42,17 @@ export default function Preview({ background, characters, enhanced, textType, te
 
     // getBackground converts human readable background names into background file names
     const getBackground = () => {
-        return background
+        const convertedBackground = background.toLowerCase().replace(' - ', '').replace(' ', '_')
+        const fileName = `../assets/${convertedBackground}.png`
+        return (
+            <>
+                <Image 
+                src={BACKGROUNDIMAGES[convertedBackground]}
+                width={500}
+                height={500} 
+                alt={background}/>
+            </>
+        )
     }
 
     // getCharacter converts human readable character objects into file names
@@ -47,10 +63,13 @@ export default function Preview({ background, characters, enhanced, textType, te
     }
 
     return (
+        <Stack>
+        <h1>Preview:</h1>
         <Container fixed>
+            { textType === TEXTDISPLAYTYPES[0] ? <Overlay text={text} /> : <Speech text={text} /> }
             {getBackground()}
             {getCharacter(characters[0])}
-            { textType === TEXTDISPLAYTYPES[0] ? <Overlay text={text} /> : <Speech text={text} /> }
         </Container>
+        </Stack>
     )
 }
